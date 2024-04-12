dbe7d3e11fb2afbbd2ca007336c37e3425029289
shabon sharipov <shabon@snoonu.com>
2024-04-12 22:51:33 +0500

    Added mud blazor

---
+++ MiddlewareBetweenApis/ResponseModel/CarResponseModels.cs
@@ -0,0 +1,36 @@
+﻿namespace MiddlewareBetweenApis.ResponseModel;
+
+public class CarResponseModels
+{
+    public long Id { get; set; }
+    
+    public string VINCode { get; set; } = string.Empty;
+
+    public string DateOfPablisher { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
+
+    public decimal EngineCapacity { get; set; }
+
+    public decimal Mileage { get; set; }
+
+    public bool IsRastamogeno { get; set; }
+
+    public List<string> Images { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public string FuelType { get; set; } = string.Empty;
+
+    public string Transmission { get; set; } = string.Empty;
+
+    public string CarBody { get; set; } = string.Empty;
+
+    public int UserId { get; set; }
+    
+    public long MarkaId { get; set; }
+
+    public long ModelId { get; set; }
+
+    public string Color { get; set; } = string.Empty;
+}
--- Sughd.Auto.Admin/Program.cs
+++ Sughd.Auto.Admin/Program.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MiddlewareBetweenApis.Services;
+using MudBlazor.Services;
 using Sughd.Auto.Admin;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped<ICarService, CarService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
